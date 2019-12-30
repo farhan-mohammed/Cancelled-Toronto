e38dfe6bd9c114a6458114423b029d71ade54531
Farhan Mohammed <farhanhm12@gmail.com>
2019-12-30 18:32:50 -0500

    React migration

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Delete from './delete';
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<Delete />
+			</div>
+		);
+	}
+}
