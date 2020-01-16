7852541986e07be1798e8a6c1140e54238f7f4fa
Farhan Mohammed <farhanhm12@gmail.com>
2020-01-15 20:54:18 -0500

    backup

--- src/components/Years/Year1960.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Year1960 extends Component {
-	render() {
-		return <div>1960s</div>;
-	}
-}
--- src/components/Years/Year1970.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Year1970 extends Component {
-	render() {
-		return <div>1970s</div>;
-	}
-}
