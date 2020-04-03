daeced0b8637137d26b32d60a929cb1036fa644f
Farhan Mohammed <farhanhm12@gmail.com>
2020-04-03 19:48:59 -0400

    Update WebsiteWrapper.js

--- components/WebsiteWrapper.js
+++ components/WebsiteWrapper.js
@@ -124,7 +124,7 @@ export default class mainSite extends Component {
 			<div className={containerWrapper}>
 				{/** There are meta properties that are stored in the Head of the page */}
 				<Head>
-					<meta property="og:image" content="/images/back.jpg" />
+					<meta property="og:image" content="/images/back-min.jpg" />
 					<link rel="icon" type="image/png" href="/images/thumb.png" />
 					<link href="/styles.css" rel="stylesheet" />
 					<script src="https://www.w3counter.com/tracker.js?id=130470" />
